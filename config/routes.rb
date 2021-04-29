d90cc8906728f31d8e1d5c633e8f2eff2f0f217c
vagrant <sribas@gmail.com>
2021-04-29 02:25:48 +0000

    Add about us page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  resources :about, only: [:index]
 
   resource :cart, only: [:show] do
     post   :add_item
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
   end
 
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
