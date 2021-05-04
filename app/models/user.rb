de38a9d0003eb645035ce8458a582f1fee5aa7e8
vagrant <sribas@gmail.com>
2021-05-04 04:11:23 +0000

    Add user model field validation. Add specs for adding and authenticating users with same emails, upper and lower cases.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,17 @@ class User < ActiveRecord::Base
   validates :first_name, presence: true
   validates :last_name, presence: true
 
-  validates :password, presence: true
-  validates :password, length: { in: 3..10 }
-  validates :password_confirmation, presence: true
+  validates :password, presence: true, length: { minimum: 3 }, confirmation: true
+  validates :password_confirmation, presence: true, length: { minimum: 3 }
   
-  validates :email, presence: true
-  validates :email, uniqueness: { case_sensitive: false }
+  validates :email, presence: true, uniqueness: { case_sensitive: false }
+
+  before_save :sanitize_email
+
+  def sanitize_email
+    self.email.downcase!
+    self.email.strip!
+  end
 
   def authenticate_with_credentials(email, password)
     user = User.find_by_email(email.strip.downcase); 
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,23 +10,61 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
 
+    it "successfuly create a new user when all fields are provided" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user.valid?
+      expect(user.errors.full_messages.empty?).to be true
+    end
+
+    it 'password and password_confirmation should match' do
+      user = User.new(password: '123456',  password_confirmation: '123456')
+      user.valid?
+      expect(user.password).to eq(user.password_confirmation)
+    end
+
     it "successfuly create a new user when providing password and password_confirmation" do
       user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user.valid?
       expect(user.errors.full_messages).to_not include("Password confirmation can't be blank","Password can't be blank")
     end
 
     it "fail to create a new user when not providing password" do
       user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: nil, password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user.valid?
       expect(user.errors.full_messages).to include("Password can't be blank")
     end
 
     it "fail to create a new user when not providing password confirmation" do
       user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: nil, email: 'lucasribas1@email.com'})
+      user.valid?
       expect(user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
+
+    it "successfuly create a new user when providing a unique email address" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: nil, password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user.valid?
+      expect(user.errors.full_messages).to_not include("Email has already been taken")
+    end
+
+    it "fail to create a new user when not providing a unique email address (both lower case)" do
+      user1 = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user2 = User.create({first_name: 'Test', last_name: 'User', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      expect(user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it "fail to create a new user when not providing a unique email address (lower/upper case)" do
+      user1 = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user2 = User.create({first_name: 'Test', last_name: 'User', password: '123456', password_confirmation: '123456', email: 'LUCASRIBAS1@email.com'})
+      expect(user2.errors.full_messages).to include("Email has already been taken")
+    end
+
+    it ".authenticate_with_credentials" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      user.valid?
+      expect(user.authenticate_with_credentials("LUCASRIBAS1@email.com","123456")).to be_truthy
     end
 
   end
 
 
 end
-
