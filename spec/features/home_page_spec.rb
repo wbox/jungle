7dfb2cb20a965423889228a540e97a010360f3a8
vagrant <sribas@gmail.com>
2021-05-04 19:14:56 +0000

    Add category and products to home_page_spec

--- spec/features/home_page_spec.rb
+++ spec/features/home_page_spec.rb
@@ -2,6 +2,23 @@ require 'rails_helper'
 
 RSpec.feature "Visitor navigates to home page", type: :feature, js: true do
 
+
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    10.times do |n|
+      @category.product.create!(
+        name: Faker::Hipster.sentence(3)
+        description: Faker::Hipster.paragraph(4),
+        quantity: 10,
+        price: 64.99
+      )
+    end
+
+  end
+
+
+
   scenario "They see all products" do
     # ACT
     visit root_path
