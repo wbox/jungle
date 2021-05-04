29281a51098fe331d80b0e8e71188cf847e9ad0e
vagrant <sribas@gmail.com>
2021-05-04 00:41:05 +0000

    Add field validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,18 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  # validates :name, presence 
+  validates :name, presence: true
+
+  validates :password, presence: true
+  validates :password, length: { 6..20 }
+  
+  validates :password_confirmation, presence: true
+  
+  validates :email, presence: true
+  validates :email, { case_sensitive: false }
+  validates :email, uniqueness: true
+
+
+
+
 end
