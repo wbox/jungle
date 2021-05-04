e8c837bf882f0854d91f3194f4c82e333a67f9f2
vagrant <sribas@gmail.com>
2021-05-04 01:04:14 +0000

    Add last_name to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210504005051) do
+ActiveRecord::Schema.define(version: 20210504010150) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 20210504005051) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.string   "last_name"
   end
 
   add_foreign_key "line_items", "orders"
