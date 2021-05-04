4cb576ec4260ea4c5342ad83b0c5fea3661baae4
vagrant <sribas@gmail.com>
2021-05-04 03:25:09 +0000

    add password and password confirmation specs

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,10 +10,20 @@ RSpec.describe User, type: :model do
 
   describe 'Validations' do
 
-    it "successfuly create a new user when providing password and password_confirmation"
-    user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas@email.com'})
-    p user.errors_full_messages
-    #expect(user.errors.full_messages)
+    it "successfuly create a new user when providing password and password_confirmation" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      expect(user.errors.full_messages).to_not include("Password confirmation can't be blank","Password can't be blank")
+    end
+
+    it "fail to create a new user when not providing password" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: nil, password_confirmation: '123456', email: 'lucasribas1@email.com'})
+      expect(user.errors.full_messages).to include("Password can't be blank")
+    end
+
+    it "fail to create a new user when not providing password confirmation" do
+      user = User.create({first_name: 'Lucas', last_name: 'Ribas', password: '123456', password_confirmation: nil, email: 'lucasribas1@email.com'})
+      expect(user.errors.full_messages).to include("Password confirmation can't be blank")
+    end
 
   end
 
